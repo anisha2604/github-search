c6afff0844c3551ccb45d31ef33284667a57e096
Anisha Tulika <anisha2604@gmail.com>
2016-09-07 20:21:58 +0530

    Displaying Open Issues for the Selected Repository
    The page now displays the open issues created on the current day for
    the selected repository

--- script.js
+++ script.js
@@ -16,5 +16,43 @@ $('#typeahead-input').typeahead({
         return process(repo);
       }
     });
+  },
+  afterSelect: function (repo) {
+    console.log(repo);
+    var repoName = repo.name;
+    var userName = repo.owner.login;
+
+    $(".repo").toggleClass("hidden");
+
+    $("#repo-name").text(repoName);
+
+    var date = new Date();
+    var formatDate = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
+    var githubIssuesAPI = "https://api.github.com/search/issues?q=state:open+repo:" + repoName + "type:issue+user:" + userName + "+created:" + formatDate + "&sort=created&order=asc";
+
+    console.log(githubIssuesAPI);
+
+    $.ajax({
+      url: githubIssuesAPI,
+      dataType: 'jsonp',
+
+      success: function (results) {
+        var issues = results.data.items;
+        console.log(issues.length);
+
+        if (issues.length != 0) {
+          $("#issues-list").html("");
+          for (var i=0; i<issues.length; i++) {
+            var issue = issues[i];
+            console.log(issue.title);
+
+            $("#issues-list").append("<li>"+issue.title+"</li>");
+          }
+        } else {
+          $("#issues-list").html("").append("<li>No Issues for Today</li>");
+        }
+      }
+    });
+
   }
 });
